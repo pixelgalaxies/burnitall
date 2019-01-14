0124f5b1f9cd4095bf20505ea936a136a60b1ac2
pixelgalaxies <pixel.galaxies@gmail.com>
2019-01-13 21:11:14 -1000

    lana quote

--- functions.js
+++ functions.js
@@ -1,49 +1,64 @@
 var quotes = [];
 quotes[0] = {
 	quote: "Burn it all.",
-	source: "-Ronald Bartel, Backdraft, 1991"};
+	source: "-Ronald Bartel, Backdraft, 1991"
+};
 quotes[1] = {
 	quote: "If all else fails, use fire.",
-	source: "-Zelda II: The Adventure of Link, 1987"};
+	source: "-Zelda II: The Adventure of Link, 1987"
+};
 quotes[2] = {
 	quote: "I set fire to the rain and I threw us into the flames.",
-	source: "-Adele, 21, 2007"};
+	source: "-Adele, 21, 2007"
+};
 quotes[3] = {
 	quote: "I found my hate in the flames.",
-	source: "-Gangplank, League of Legends, 2015"};
+	source: "-Gangplank, League of Legends, 2015"
+};
 quotes[4] = {
 	quote: "So when you fall, I’ll take my turn and fan the flames as your blazes burn.",
-	source: "-Linkin Park, Burn It Down, 2012"};
+	source: "-Linkin Park, Burn It Down, 2012"
+};
 quotes[5] = {
 	quote: "May the bridges I burn light the way.",
-	source: "-Dylan McKay, Beverly Hills 90210, 1994"};
+	source: "-Dylan McKay, Beverly Hills 90210, 1994"
+};
 quotes[6] = {
 	quote: "Think of me and burn.",
-	source: "-Corona, The Rhythm of the Night, 1994"};
+	source: "-Corona, The Rhythm of the Night, 1994"
+};
 quotes[7] = {
 	quote: "It's better to burn than to feel that cold.",
-	source: "-Tocadisco, Better Run, 2010"};
+	source: "-Tocadisco, Better Run, 2010"
+};
 quotes[8] = {
 	quote: "I am a phoenix. Watch me fall. Watch me burn. Watch me rise.",
-	source: "-mywritingstory.tumblr.com, 2015"};
+	source: "-mywritingstory.tumblr.com, 2015"
+};
 quotes[9] = {
 	quote: "Before you play with fire, do think twice. If you get burned well, baby, don't you be surprised.",
-	source: "-Robin Schulz, Sugar, 2015"};
+	source: "-Robin Schulz, Sugar, 2015"
+};
 quotes[10] = {
 	quote: "I survived because the fire inside me burned brighter than the fire around me.",
-	source: "-Joshua Graham, Fall Out 4, 2015"};
+	source: "-Joshua Graham, Fall Out 4, 2015"
+};
 quotes[11] = {
 	quote: "Fire is catching and if we burn, you burn with us.",
-	source: "-Suzanne Collins, Mockingjay, 2010"};
+	source: "-Suzanne Collins, Mockingjay, 2010"
+};
 quotes[12] = {
 	quote: "Neither the flames nor the depths could claim me.",
-	source: "-Gangplank, League of Legends, 2015"};
+	source: "-Gangplank, League of Legends, 2015"
+};
 quotes[13] = {
 	quote: "Ready to set the world on fire?",
-	source: "-Brand, League of Legends, 2011"};
+	source: "-Brand, League of Legends, 2011"
+};
 quotes[14] = {
 	quote: "Burn  everything you love then burn the ashes.",
-	source: "-Fall Out Boy, My Songs Know What You Did In The Dark, 2013"};
+	source: "-Fall Out Boy, My Songs Know What You Did In The Dark, 2013"
+};
 quotes[15] = {
 	quote: "I crashed my car into the bridge. I watched, I let it burn.",
 	source: "-Icona Pop, I Love It, 2012"
@@ -61,8 +76,12 @@ quotes[18] = {
 	source: "-Alan Walker, Faded, 2015"
 };
 quotes[19] = {
-	quote:"I put my hands among the flames. Nothing burns.",
+	quote: "I put my hands among the flames. Nothing burns.",
 	source: "-Sylvia Plath, Poppies In July, 1962"
+};
+quotes[20] = {
+	quote: "Honey, I'm on fire, I feel it everywhere. Nothing scares me anymore.",
+	source: "-Lana Delray, Summertime Sadness, 2012"
 };
 
 var num = 0;
@@ -76,11 +95,11 @@ function quoteClick() {
 	//generates random number based on quotes array
 	num = Math.floor(Math.random() * quotes.length);
 	//sets quote using random number generated
-	quoteText.innerHTML = '"'+quotes[num].quote+'"';
+	quoteText.innerHTML = '"' + quotes[num].quote + '"';
 	//sets source using random number generated
 	sourceText.innerHTML = quotes[num].source;
 	//generates a random number based on images array
 	imgNum = Math.floor(Math.random() * images.length);
 	//sets bg image using random number generated
-	dBody.style.backgroundImage = "url("+images[imgNum]+")";
+	dBody.style.backgroundImage = "url(" + images[imgNum] + ")";
 }
